ee4922fcc40d345193d27cfb2f1ea34d053394d8
Joan Lucas <reecaptcha@gmail.com>
2024-05-31 00:46:38 -0300

    Added new entity type representation.

--- Entity/entity_type.py
+++ Entity/entity_type.py
@@ -5,6 +5,7 @@ from enum import Enum
 class EntityType(Enum):
     """Declares every entity type."""
     HEALTHY: str = '*' # type: ignore
+    IMMUNE: str = '~' # type: ignore
     INFECTED: str = 'O' # type: ignore
     DEAD: str = '+' # type: ignore
     HEALED: str = '.' # type: ignore
