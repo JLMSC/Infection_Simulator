fb1e97b0b783c0e3b9a033f01bfbe6d339d68064
Joan Lucas <reecaptcha@gmail.com>
2024-05-31 00:56:59 -0300

    Added new example of usage.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from time import sleep
 
 
+from Entity.entity import Entity
 from World.world import World
 
 world = World(shape=10)
 
 step = 0
-while len(world.get_infected_entities()) >= 1:
+target_entity_type = Entity(position=(-1, -1), is_infected=True, is_immune=False).entity_type
+while len(world.get_matching_entity_type_positions(target_entity_type=target_entity_type)) >= 1:
     # sleep(1)
     world.next_iteration()
     print(f'Step # {step} done.')
